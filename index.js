ce9744f14ae5b9edc197a6a566375a899e1f64b1
Ngecha <ngechadennis@gmail.com>
2024-07-09 07:06:37 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase;
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+setBestCustomer();
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'never bob';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "always bob";
+    console.log(leastFavoriteCustomer);
+    return leastFavoriteCustomer;
+}
+changeLeastFavoriteCustomer();
+console.log(leastFavoriteCustomer);
